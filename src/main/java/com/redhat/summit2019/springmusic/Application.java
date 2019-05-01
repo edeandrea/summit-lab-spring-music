9ae442f58ec855bee625a155cae85947e80c73b6
Eric Deandrea <edeandrea@redhat.com>
2019-05-01 16:45:37 -0400

    Update for war deployment

--- src/main/java/com/redhat/summit2019/springmusic/Application.java
+++ src/main/java/com/redhat/summit2019/springmusic/Application.java
@@ -2,11 +2,12 @@ package com.redhat.summit2019.springmusic;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.redhat.summit2019.springmusic.repositories.AlbumRepositoryPopulator;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class)
