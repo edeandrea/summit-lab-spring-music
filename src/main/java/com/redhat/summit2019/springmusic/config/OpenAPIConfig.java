2c5c1b2c15cb4420acfad73f1f7b9045acc147a0
Eric Deandrea <edeandrea@redhat.com>
2020-01-03 08:22:18 -0500

    Adding springdoc-openapi-ui dependency back in due to fix

--- src/main/java/com/redhat/summit2019/springmusic/config/OpenAPIConfig.java
+++ src/main/java/com/redhat/summit2019/springmusic/config/OpenAPIConfig.java
@@ -3,15 +3,21 @@ package com.redhat.summit2019.springmusic.config;
 import java.util.Optional;
 
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
 public class OpenAPIConfig {
 	private final Optional<BuildProperties> buildProperties;
 
 	public OpenAPIConfig(Optional<BuildProperties> buildProperties) {
 		this.buildProperties = buildProperties;
 	}
-/*
+
 	@Bean
 	public OpenAPI openApi() {
 		OpenAPI openAPI = new OpenAPI()
@@ -46,5 +52,5 @@ public class OpenAPIConfig {
 					.email("edeandrea@redhat.com")
 					.url("https://github.com/edeandrea/summit-lab-spring-music")
 			);
-	}*/
+	}
 }
